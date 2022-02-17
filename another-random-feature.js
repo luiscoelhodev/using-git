742064a778507e388f0db8a7a063a422afc6a16e
Luis Coelho <mancoeluis@gmail.com>
2022-02-17 11:14:52 -0300

    finished feature

--- another-random-feature.js
+++ another-random-feature.js
@@ -1,2 +1,3 @@
 // This is another 'fake' feature I'll be working on in this project to practice the creation of another branch.
 // Let's say I made some changes directly here in my remote repository. I'll pull it to my local computer to see it!
+// Now let's imagine I have finished this feature and I want to merge it with the main branch.
